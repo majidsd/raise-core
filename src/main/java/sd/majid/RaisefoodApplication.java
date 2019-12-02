5177749b961c39c8bd85f8a33b36cec3fe535a3b
majid <majid@majid-ThinkPad-T470s>
2019-12-02 12:41:28 +0200

    Initial commit.

---
+++ src/main/java/sd/majid/RaisefoodApplication.java
@@ -0,0 +1,13 @@
+package sd.majid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaisefoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaisefoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sd/majid/RaisefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package sd.majid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaisefoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
