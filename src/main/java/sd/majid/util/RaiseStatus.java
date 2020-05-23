96923ced061c3922add2cd53541570f17e4e5875
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-23 16:15:32 +0200

    Edit raise status from class to be enum.

--- src/main/java/sd/majid/util/RaiseStatus.java
+++ src/main/java/sd/majid/util/RaiseStatus.java
@@ -1,9 +1,45 @@
 package sd.majid.util;
 
-public class RaiseStatus {
+public enum RaiseStatus {
 	
-	public static final Integer NEW = 1;
-	public static final Integer IN_PROGRESS = 2;
-	public static final Integer DONE = 3;
+	NEW("New", (byte) 0),
+	IN_PROGRESS("In Progress", (byte) 1),
+	DONE("Done", (byte) 2),
+	CANCELED("Canceled", (byte) -1);
+	
+	private String name;
+	private Byte value;
+	
+	private RaiseStatus(String name, Byte value) {
+		this.name = name;
+		this.value = value;
+	}
 
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public Byte getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(Byte value) {
+		this.value = value;
+	}
 }
