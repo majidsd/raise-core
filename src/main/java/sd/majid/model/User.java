7f11b12110ad5465877b8638d2ee0a46d58510a4
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-23 15:59:06 +0200

    Edit user model
    - Adding createdBy, updatedBy attributes.
    - Edit status type, from Integer to Byte.

--- src/main/java/sd/majid/model/User.java
+++ src/main/java/sd/majid/model/User.java
@@ -4,10 +4,16 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "user")
@@ -17,94 +23,198 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "name")
+	@Column(name = "name", nullable = false, unique = true)
 	private String name;
 	
-	@Column(name = "phone")
+	@Column(name = "phone", nullable = false)
 	private String phone;
 	
-	@Column(name = "email")
+	@Column(name = "email", nullable = false)
 	private String email;
 	
-	@Column(name = "userName")
+	@Column(name = "userName", nullable = false, unique = true)
 	private String userName;
 	
-	@Column(name = "createdAt")
+	@Column(name = "createdAt", nullable = false)
 	private Date createdAt;
 	
-	@Column(name = "password")
+	@Column(name = "updatedAt", nullable = false)
+	private Date updatedAt;
+	
+	@Column(name = "password", nullable = false)
 	private String password;
 	
-	@Column(name = "status")
-	private Integer status; 
+	@Column(name = "status", nullable = false)
+	private Byte status;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "createdBy", nullable = true)
+	@OnDelete(action = OnDeleteAction.NO_ACTION)
+	private User createdBy;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "updatedBy", nullable = true)
+	@OnDelete(action = OnDeleteAction.NO_ACTION)
+	private User updatedBy;
 
+	/**
+	 * @return the id
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * @param id the id to set
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the phone
+	 */
 	public String getPhone() {
 		return phone;
 	}
 
+	/**
+	 * @param phone the phone to set
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * @param email the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * @return the userName
+	 */
 	public String getUserName() {
 		return userName;
 	}
 
+	/**
+	 * @param userName the userName to set
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	/**
+	 * @return the createdAt
+	 */
 	public Date getCreatedAt() {
 		return createdAt;
 	}
 
+	/**
+	 * @param createdAt the createdAt to set
+	 */
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
 
+	/**
+	 * @return the updatedAt
+	 */
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	/**
+	 * @param updatedAt the updatedAt to set
+	 */
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	/**
+	 * @return the password
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * @param password the password to set
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public Integer getStatus() {
+	/**
+	 * @return the status
+	 */
+	public Byte getStatus() {
 		return status;
 	}
 
-	public void setStatus(Integer status) {
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Byte status) {
 		this.status = status;
+	}
+	
+	/**
+	 * @return the createdBy
+	 */
+	public User getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * @param createdBy the createdBy to set
+	 */
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * @return the updatedBy
+	 */
+	public User getUpdatedBy() {
+		return updatedBy;
+	}
+
+	/**
+	 * @param updatedBy the updatedBy to set
+	 */
+	public void setUpdatedBy(User updatedBy) {
+		this.updatedBy = updatedBy;
 	}
 
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + ", userName=" + userName
-				+ ", createdAt=" + createdAt + ", password=" + password + ", status=" + status + "]";
+				+ ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + ", password=" + password + ", status="
+				+ status + ", createdBy=" + createdBy + ", updatedBy=" + updatedBy + "]";
 	}
 }
