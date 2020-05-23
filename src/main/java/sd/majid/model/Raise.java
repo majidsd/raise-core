121cf274f844d904ca32837ee0a02f3f08d138f6
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-05-23 16:00:52 +0200

    Edit Raise model
    - Adding organization attributes.
    - Change status type from Integer to Byte.

--- src/main/java/sd/majid/model/Raise.java
+++ src/main/java/sd/majid/model/Raise.java
@@ -23,111 +23,199 @@ public class Raise {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "name")
+	@Column(name = "name", nullable = false)
 	private String name;
 	
-	@Column(name = "phone")
+	@Column(name = "phone", nullable = false)
 	private String phone;
 	
-	@Column(name = "description")
+	@Column(name = "description", nullable = true)
 	private String description;
 	
-	@Column(name = "longitude")
+	@Column(name = "longitude", nullable = false)
 	private String longitude;
 	
-	@Column(name = "latitude")
+	@Column(name = "latitude", nullable = false)
 	private String latitude;
 	
-	@Column(name = "status")
-	private Integer status;
+	@Column(name = "status", nullable = false)
+	private Byte status;
 	
-	@Column(name = "createdAt")
+	@Column(name = "createdAt", nullable = false)
 	private Date createdAt;
+	
+	@Column(name = "updatedAt", nullable = false)
+	private Date updatedAt;
 	
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@JoinColumn(name = "user", nullable = true)
 	@OnDelete(action = OnDeleteAction.NO_ACTION)
 	private User user;
-
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	@JoinColumn(name = "organization", nullable = true)
+	@OnDelete(action = OnDeleteAction.NO_ACTION)
+	private Organization organization;
+	
+	/**
+	 * @return the id
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * @param id the id to set
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the phone
+	 */
 	public String getPhone() {
 		return phone;
 	}
 
+	/**
+	 * @param phone the phone to set
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+	/**
+	 * @return the description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * @param description the description to set
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+	/**
+	 * @return the longitude
+	 */
 	public String getLongitude() {
 		return longitude;
 	}
 
+	/**
+	 * @param longitude the longitude to set
+	 */
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
 
+	/**
+	 * @return the latitude
+	 */
 	public String getLatitude() {
 		return latitude;
 	}
 
+	/**
+	 * @param latitude the latitude to set
+	 */
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
 
-	public Integer getStatus() {
+	/**
+	 * @return the status
+	 */
+	public Byte getStatus() {
 		return status;
 	}
 
-	public void setStatus(Integer status) {
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Byte status) {
 		this.status = status;
 	}
 
+	/**
+	 * @return the createdAt
+	 */
 	public Date getCreatedAt() {
 		return createdAt;
 	}
 
+	/**
+	 * @param createdAt the createdAt to set
+	 */
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-	
-	
 
+	/**
+	 * @return the updatedAt
+	 */
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	/**
+	 * @param updatedAt the updatedAt to set
+	 */
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	/**
+	 * @return the user
+	 */
 	public User getUser() {
 		return user;
 	}
 
+	/**
+	 * @param user the user to set
+	 */
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	/**
+	 * @return the organization
+	 */
+	public Organization getOrganization() {
+		return organization;
+	}
+
+	/**
+	 * @param organization the organization to set
+	 */
+	public void setOrganization(Organization organization) {
+		this.organization = organization;
 	}
 
 	@Override
 	public String toString() {
 		return "Raise [id=" + id + ", name=" + name + ", phone=" + phone + ", description=" + description
 				+ ", longitude=" + longitude + ", latitude=" + latitude + ", status=" + status + ", createdAt="
-				+ createdAt + ", user=" + user + "]";
+				+ createdAt + ", updatedAt=" + updatedAt + ", user=" + "" + ", organization=" + organization + "]";
 	}
-
+	
 }
