c63fe9f34b0085aaaacd289a14464fc779847934
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-12-20 00:11:15 +0100

    Adding Raise Api Controller

---
+++ src/main/java/sd/majid/controller/api/RaiseApiController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package sd.majid.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import sd.majid.service.IRaiseService;
+
+/**
+ * @author MaJiD
+ *
+ */
+public class RaiseApiController {
+	@Autowired
+	private IRaiseService raiseService;
+}
