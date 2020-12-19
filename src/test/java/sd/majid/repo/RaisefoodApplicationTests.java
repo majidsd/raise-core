4f754e11bfc5363fc79957eff014e4d2a8ad33f3
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-12-20 00:06:57 +0100

    Edit RaisefoodApplicationTests for repo
    Edit testAddRaise and testAddUser by adding some properties

--- src/test/java/sd/majid/repo/RaisefoodApplicationTests.java
+++ src/test/java/sd/majid/repo/RaisefoodApplicationTests.java
@@ -1,11 +1,16 @@
 package sd.majid.repo;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import sd.majid.model.Raise;
 import sd.majid.model.User;
+import sd.majid.util.HashData;
+import sd.majid.util.UserStatus;
 
 @SpringBootTest
 class RaisefoodApplicationTests {
@@ -35,6 +40,14 @@ class RaisefoodApplicationTests {
 		user.setUserName("majid");
 		user.setPhone("0123456789");
 		user.setEmail("majid@test.sudan");
+		user.setCreatedAt(new Date());
+		user.setUpdatedAt(new Date());
+		user.setStatus(UserStatus.ACTIVE.getValue());
+		try {
+			user.setPassword(HashData.sha1("test"));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 		userRepository.save(user);
 	}
 
