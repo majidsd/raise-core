43711cd16e974f467f261f123e1471567087efcd
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-12-19 23:57:18 +0100

    Edit UserDto class
    Adding new properties to the class

--- src/main/java/sd/majid/dto/UserDto.java
+++ src/main/java/sd/majid/dto/UserDto.java
@@ -3,6 +3,8 @@
  */
 package sd.majid.dto;
 
+import java.util.Date;
+
 /**
  * @author MaJiD
  *
@@ -10,6 +12,11 @@ package sd.majid.dto;
 public class UserDto {
 	private Long id;
 	private String name;
+	private String phone;
+	private String email;
+	private Date createdAt;
+	private Date updatedAt;
+	private Byte status;
 	/**
 	 * @return the id
 	 */
@@ -34,7 +41,69 @@ public class UserDto {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the createdAt
+	 */
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	/**
+	 * @param createdAt the createdAt to set
+	 */
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	/**
+	 * @return the updatedAt
+	 */
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	/**
+	 * @param updatedAt the updatedAt to set
+	 */
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	/**
+	 * @return the status
+	 */
+	public Byte getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Byte status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "UserDto [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + ", createdAt="
+				+ createdAt + ", updatedAt=" + updatedAt + ", status=" + status + "]";
+	}
 }
