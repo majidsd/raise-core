c67284f2df7373b1730a53e24425bcba04afdd7c
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-12-20 00:10:27 +0100

    Adding Organization Api Controller

---
+++ src/main/java/sd/majid/controller/api/OrganizationApiController.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package sd.majid.controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import sd.majid.service.IOrganizationService;
+
+/**
+ * @author MaJiD
+ *
+ */
+public class OrganizationApiController {
+	@Autowired
+	private IOrganizationService organizationService;
+	
+}
