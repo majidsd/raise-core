f7b07478f8172e4379e4aa6187377e701e649e33
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2020-12-20 01:27:44 +0100

    Adding the new user services to user api controller
    Adding updateUser, activateUser, revokeUser and deleteUser

--- src/main/java/sd/majid/controller/api/UserApiController.java
+++ src/main/java/sd/majid/controller/api/UserApiController.java
@@ -4,12 +4,18 @@
 package sd.majid.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sd.majid.dto.UserDto;
+import sd.majid.model.User;
+import sd.majid.response.BaseResponse;
 import sd.majid.response.ListResponse;
 import sd.majid.response.ObjectResponse;
 import sd.majid.service.IUserService;
@@ -34,6 +40,36 @@ public class UserApiController {
 	@GetMapping("/get/{userId}")
 	public ObjectResponse<UserDto> getUser(@PathVariable("userId") Long userId){
 		ObjectResponse<UserDto> response = userService.getUserById(userId);
+		return response;
+	}
+	
+	@PostMapping("/save")
+	public ObjectResponse<UserDto> saveUser(@RequestBody User user){
+		ObjectResponse<UserDto> response = userService.addUser(user);
+		return response;
+	}
+	
+	@PutMapping("/update")
+	public ObjectResponse<UserDto> updateUser(@RequestBody User user){
+		ObjectResponse<UserDto> response = userService.updateUser(user);
+		return response;
+	}
+	
+	@PutMapping("/revoke")
+	public BaseResponse revokeUser(@RequestBody User user){
+		BaseResponse response = userService.revokeUser(user);
+		return response;
+	}
+	
+	@PutMapping("/activate")
+	public ObjectResponse<UserDto> activateUser(@RequestBody User user){
+		ObjectResponse<UserDto> response = userService.activateUser(user);
+		return response;
+	}
+	
+	@DeleteMapping("/delete")
+	public BaseResponse deleteUser(@RequestBody User user) {
+		BaseResponse response = userService.deleteUser(user);
 		return response;
 	}
 }
