f6e13b32753f69bfc81e1905c5974a07412c1585
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2021-02-25 00:17:19 +0100

    Adding UserService functions to unit test

--- src/test/java/sd/majid/service/UserServiceUnitTests.java
+++ src/test/java/sd/majid/service/UserServiceUnitTests.java
@@ -8,7 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import sd.majid.dto.UserDto;
+import sd.majid.model.Organization;
+import sd.majid.model.User;
+import sd.majid.response.ListResponse;
 import sd.majid.response.ObjectResponse;
+import sd.majid.util.HashData;
+import sd.majid.util.UserType;
 
 
 /**
@@ -22,9 +27,166 @@ public class UserServiceUnitTests {
 	private IUserService userService;
 	
 	@Test
-	public void testGetUser() {
-		ObjectResponse<UserDto> userDto = userService.getUserById(1L);
-		System.out.println(userDto.toString());
+	public void testAddUser() {
+		// Asses
+		User creater = new User();
+		creater.setId(3L);
+		Organization org = new Organization();
+		org.setId(1L);
+		
+		User user = new User();
+		user.setName("A B");
+		user.setPhone("0123455789");
+		user.setEmail("majid@t.sd");
+		user.setUserName("abc");
+		user.setType(UserType.NGO_MANAGEMENT);
+		user.setCreatedBy(creater);
+		user.setUpdatedBy(creater);
+		user.setOrganization(org);
+		try {
+			user.setPassword(HashData.sha1("sssss"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Act
+		ObjectResponse<UserDto> response = userService.addUser(user);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getUserName() + " - " + response.getDto().getEmail() + " - " + response.getDto().getStatus());
 	}
 	
+	
+	@Test
+	public void testActivateUser() {
+		// Asses
+		User user = userService.getUserObjectById(2L).getDto();
+		
+		// Act
+		ObjectResponse<UserDto> response = userService.activateUser(user);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getUserName() + " - " + response.getDto().getEmail() + " - " + response.getDto().getStatus());
+	}
+	
+	
+	@Test
+	public void testRevokeUser() {
+		// Asses
+		User user = userService.getUserObjectById(3L).getDto();
+		
+		// Act
+		ObjectResponse<UserDto> response = userService.revokeUser(user);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getUserName() + " - " + response.getDto().getEmail() + " - " + response.getDto().getStatus());
+	}
+	
+	
+	@Test
+	public void testDeleteUser() {
+		// Asses
+		User user = userService.getUserObjectById(3L).getDto();
+		
+		// Act
+		ObjectResponse<UserDto> response = userService.deleteUser(user);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getUserName() + " - " + response.getDto().getEmail() + " - " + response.getDto().getStatus());
+	}
+	
+	
+	@Test
+	public void testUpdateUserData() {
+		// Asses
+		User user = userService.getUserObjectById(1L).getDto();
+		user.setName("Abdalmajid A. Ishag");
+		
+		// Act
+		ObjectResponse<UserDto> response = userService.updateUserData(user);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getUserName() + " - " + response.getDto().getEmail() + " - " + response.getDto().getStatus());
+	}
+	
+	
+	@Test
+	public void testGetUserById() {
+		// Act
+		ObjectResponse<UserDto> response = userService.getUserById(1L);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getUserName() + " - " + response.getDto().getEmail() + " - " + response.getDto().getStatus());
+	}
+	
+	
+	@Test
+	public void testGetUserObjectById() {
+		// Act
+		ObjectResponse<User> response = userService.getUserObjectById(1L);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getUserName() + " - " + response.getDto().getEmail() + " - " + response.getDto().getStatus());
+	}
+	
+	
+	@Test
+	public void testGetAllActivateUsers() {
+		// Act
+		ListResponse<UserDto> response = userService.getAllActivateUsers();
+		
+		// Assert
+		System.out.println(response);
+		for(UserDto dto : response.getDtos()) {
+			System.out.println(dto.getName() + " - " + dto.getUserName() + " - " + dto.getEmail() + " - " + dto.getStatus());
+		}
+	}
+	
+	
+	@Test
+	public void testGetAllDeclinedUsers() {
+		// Act
+		ListResponse<UserDto> response = userService.getAllDeclinedUsers();
+		
+		// Assert
+		System.out.println(response);
+		for(UserDto dto : response.getDtos()) {
+			System.out.println(dto.getName() + " - " + dto.getUserName() + " - " + dto.getEmail() + " - " + dto.getStatus());
+		}
+	}
+	
+	@Test
+	public void testGetOrganizationUsers() {
+		// Asses
+		Organization organization = new Organization();
+		organization.setId(1L);
+		
+		// Act
+		ListResponse<UserDto> response = userService.getOrganizationUsers(organization);
+		
+		// Assert
+		System.out.println(response);
+		for(UserDto dto : response.getDtos()) {
+			System.out.println(dto.getName() + " - " + dto.getUserName() + " - " + dto.getEmail() + " - " + dto.getStatus());
+		}
+	}
+	
+	@Test
+	public void testGetAllUsers() {
+		// Act
+		ListResponse<UserDto> response = userService.getAllUsers();
+		
+		// Assert
+		System.out.println(response);
+		for(UserDto dto : response.getDtos()) {
+			System.out.println(dto.getName() + " - " + dto.getUserName() + " - " + dto.getEmail() + " - " + dto.getStatus());
+		}
+	}
 }
