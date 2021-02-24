f036b75f908b86494b99e0ac28a60e5a2dffa4eb
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2021-02-24 20:09:03 +0100

    Adding all functions of RaiseService to RaiseService unit test class

--- src/test/java/sd/majid/service/RaiseServiceUnitTests.java
+++ src/test/java/sd/majid/service/RaiseServiceUnitTests.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import sd.majid.dto.RaiseDto;
+import sd.majid.model.Organization;
 import sd.majid.model.Raise;
+import sd.majid.model.User;
 import sd.majid.response.ListResponse;
 import sd.majid.response.ObjectResponse;
 
@@ -16,120 +18,353 @@ public class RaiseServiceUnitTests {
 	private IRaiseService raiseService;
 	
 	@Test
-	public void testAddRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setName("Abdalmajid Abdalla");
-		raiseDto.setPhone("0912134212");
-		raiseDto.setLatitude("30");
-		raiseDto.setLongitude("30");
-		raiseDto.setDescription("I have food for you.");
-		raiseDto.setUser_id(1L);
+	public void testAddRaise() {
 		
-		ObjectResponse<Raise> response = raiseService.addRaise(raiseDto);
+		// Arrange
+		Raise raise = new Raise();
+		User createdBy = new User();
+		createdBy.setId(1L);
+		
+		raise.setName("Mohammed Salah");
+		raise.setPhone("0123456712");
+		raise.setLatitude("20");
+		raise.setLongitude("30");
+		raise.setDescription("I have food for 200 person in my home.");
+		raise.setCreatedBy(createdBy);
+		
+		// Act
+		ObjectResponse<RaiseDto> response = raiseService.addRaise(raise);
+		
+		// Assert
 		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getPhone() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getRaiseByIdUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setId(1L);
-		ObjectResponse<Raise> response = raiseService.getRaiseById(raiseDto);
-		System.out.println(response.getResponseMessage());
-	}
-	
-	@Test
-	public void cancelRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setId(2L);
-		ObjectResponse<Raise> response = raiseService.cancelRaise(raiseDto);
-		System.out.println(response.getResponseMessage());
-	}
-	
-	@Test
-	public void inProgressRaiseRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setId(2L);
-		raiseDto.setOrganization_id(1L);
-		ObjectResponse<Raise> response = raiseService.inProgressRaise(raiseDto);
-		System.out.println(response.getResponseMessage());
-	}
-	
-	@Test
-	public void completeRaiseRaiseRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setId(3L);
-		ObjectResponse<Raise> response = raiseService.completeRaise(raiseDto);
-		System.out.println(response.getResponseMessage());
-	}
-	
-	@Test
-	public void testGetAllRaisesUnitTest() {
-		ListResponse<Raise> response = raiseService.getAllRaises();
-		System.out.println(response.getResponseMessage() + " - " + response.getDtos());
-	}
-	
-	@Test
-	public void getNewRaiseUnitTest() {
-		ListResponse<Raise> response = raiseService.getNewRaise();
-		System.out.println(response.getResponseMessage() + " - " + response.getDtos());
-	}
-	
-	@Test
-	public void getDoneRaiseUnitTest() {
-		ListResponse<Raise> response = raiseService.getDoneRaise();
-		System.out.println(response.getResponseMessage() + " - " + response.getDtos());
-	}
-	
-	@Test
-	public void getNotDoneRaiseUnitTest() {
-		ListResponse<Raise> response = raiseService.getNotDoneRaise();
+	public void testInProgressRaise() {
+		// Arrange
+		Raise raise = raiseService.getRaiseObjectById(4L).getDto();
+		
+		Organization org = new Organization();
+		org.setId(1L);
+		raise.setOrganization(org);
+		
+		// Act
+		ObjectResponse<RaiseDto> response = raiseService.inProgressRaise(raise);
+		
+		// Assert
 		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getPhone() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getInProgressRaiseUnitTest() {
-		ListResponse<Raise> response = raiseService.getInProgressRaise();
+	public void testAssignRaise() {
+		// Arrange
+		Raise raise = raiseService.getRaiseObjectById(1L).getDto();
+		
+		User user = new User();
+		user.setId(1L);
+		raise.setPickUp(user);
+		
+		// Act
+		ObjectResponse<RaiseDto> response = raiseService.assignRaise(raise);
+		
+		// Assert
 		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getPhone() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getMyAllRaisesUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setUser_id(1L);
-		ListResponse<Raise> response = raiseService.getMyAllRaises(raiseDto);
-		System.out.println(response.getResponseMessage());
+	public void testCompleteRaise() {
+		// Arrange
+		Raise raise = raiseService.getRaiseObjectById(1L).getDto();
+		
+		// Act
+		ObjectResponse<RaiseDto> response = raiseService.completeRaise(raise);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getPhone() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getMyNotDoneRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setUser_id(1L);
-		ListResponse<Raise> response = raiseService.getMyNotDoneRaise(raiseDto);
-		System.out.println(response.getResponseMessage());
+	public void testCancelRaise() {
+		// Arrange
+		Raise raise = raiseService.getRaiseObjectById(5L).getDto();
+		
+		// Act
+		ObjectResponse<RaiseDto> response = raiseService.cancelRaise(raise);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getPhone() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getMyInProgressRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setUser_id(1L);
-		ListResponse<Raise> response = raiseService.getMyInProgressRaise(raiseDto);
-		System.out.println(response.getResponseMessage());
+	public void testGetRaiseById() {
+		// Act
+		ObjectResponse<RaiseDto> response = raiseService.getRaiseById(1L);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getPhone() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getMyDoneRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setUser_id(1L);
-		ListResponse<Raise> response = raiseService.getMyDoneRaise(raiseDto);
-		System.out.println(response.getResponseMessage());
+	public void testGetRaiseObjectById() {
+		// Act
+		ObjectResponse<Raise> response = raiseService.getRaiseObjectById(1L);
+				
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getPhone() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getMyNewRaiseUnitTest() {
-		RaiseDto raiseDto = new RaiseDto();
-		raiseDto.setUser_id(1L);
-		ListResponse<Raise> response = raiseService.getMyNewRaise(raiseDto);
-		System.out.println(response.getResponseMessage());
+	public void testGetOrganizationInProgressRaises() {
+		// Arrange
+		Organization organization = new Organization();
+		organization.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getOrganizationInProgressRaises(organization);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
 	}
 	
+	@Test
+	public void testGetOrganizationDoneRaises() {
+		// Arrange
+		Organization organization = new Organization();
+		organization.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getOrganizationDoneRaises(organization);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetOrganizationCanceledRaises() {
+		// Arrange
+		Organization organization = new Organization();
+		organization.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getOrganizationCanceledRaises(organization);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetOrganizationNotDoneRaises() {
+		// Arrange
+		Organization organization = new Organization();
+		organization.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getOrganizationNotDoneRaises(organization);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetOrganizationAllRaises() {
+		// Arrange
+		Organization organization = new Organization();
+		organization.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getOrganizationAllRaises(organization);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetUserNewRaises() {
+		// Assert
+		User user = new User();
+		user.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getUserNewRaises(user);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetUserInProgressRaises() {
+		// Assert
+		User user = new User();
+		user.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getUserInProgressRaises(user);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetUserDoneRaises() {
+		// Assert
+		User user = new User();
+		user.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getUserDoneRaises(user);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetUserCanceledRaises() {
+		// Assert
+		User user = new User();
+		user.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getUserCanceledRaises(user);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetUserNotDoneRaises() {
+		// Assert
+		User user = new User();
+		user.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getUserNotDoneRaises(user);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetUserAllRaisess() {
+		// Assert
+		User user = new User();
+		user.setId(1L);
+		
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getUserAllRaises(user);
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetAllNewRaises() {
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getAllNewRaises();
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetAllInProgressRaises() {
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getAllInProgressRaises();
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetAllDoneRaises() {
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getAllDoneRaises();
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetAllCanceledRaises() {
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getAllCanceledRaises();
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetAllNotDoneRaises() {
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getAllNotDoneRaises();
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
+	
+	@Test
+	public void testGetAllRaises() {
+		// Act
+		ListResponse<RaiseDto> response = raiseService.getAllRaises();
+		
+		// Assert
+		System.out.println(response);
+		for(RaiseDto raiseDto : response.getDtos()) {
+			System.out.println(raiseDto.getName() + " - "+ raiseDto.getDescription() + " - " + raiseDto.getPhone());
+		}
+	}
 }
