48fcb7e0220e91bc9277b005d45799b839e77938
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2021-02-24 20:03:02 +0100

    fix naming inside query for Raise repo.

--- src/main/java/sd/majid/repo/RaiseRepository.java
+++ src/main/java/sd/majid/repo/RaiseRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import sd.majid.model.Organization;
 import sd.majid.model.Raise;
 import sd.majid.model.User;
 import sd.majid.util.RaiseStatus;
@@ -18,19 +19,28 @@ import sd.majid.util.RaiseStatus;
  */
 public interface RaiseRepository extends JpaRepository<Raise, Long> {
 	
-	@Query("From Raise where user = ?1")
-	public List<Raise> getMyAllRaises(User user);
+	@Query("From Raise where createdBy = ?1 and status = ?2")
+	public List<Raise> getUserRaisesByStatus(User createdBy, RaiseStatus status);
 	
-	@Query("From Raise where status != ?1")
-	public List<Raise> getRaiseByNotStatus(RaiseStatus status);
+	@Query("From Raise where createdBy = ?1 and status != ?2")
+	public List<Raise> getUserRaisesByNotStatus(User createdBy, RaiseStatus status);
+	
+	@Query("From Raise where createdBy = ?1")
+	public List<Raise> getUserRaises(User createdBy);
+	
+	@Query("From Raise where organization =?1 and status = ?2")
+	public List<Raise> getOrganizationRaiseByStatus(Organization organization, RaiseStatus status);
+	
+	@Query("From Raise where organization =?1 and status != ?2")
+	public List<Raise> getOrganizationRaiseByNotStatus(Organization organization, RaiseStatus status);
+	
+	@Query("From Raise where organization =?1")
+	public List<Raise> getOrganizationRaises(Organization organization);
 	
 	@Query("From Raise where status = ?1")
 	public List<Raise> getRaiseByStatus(RaiseStatus status);
 	
-	@Query("From Raise where status = ?1 and user = ?2")
-	public List<Raise> getRaiseByUserAndStatus(User user, RaiseStatus status);
+	@Query("From Raise where status != ?1")
+	public List<Raise> getRaiseByNotStatus(RaiseStatus status);
 	
-	@Query("From Raise where status != ?1 and user = ?2")
-	public List<Raise> getRaiseByUserAndNotStatus(User user, RaiseStatus status);
-
 }
