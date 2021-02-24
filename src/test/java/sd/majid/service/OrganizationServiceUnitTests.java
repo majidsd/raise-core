7de9b609a80029866f90a9acdc110252f67f51a7
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2021-02-24 22:05:03 +0100

    Add all functions unit test of OrganizationService

--- src/test/java/sd/majid/service/OrganizationServiceUnitTests.java
+++ src/test/java/sd/majid/service/OrganizationServiceUnitTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import sd.majid.dto.OrganizationDto;
 import sd.majid.model.Organization;
+import sd.majid.model.User;
 import sd.majid.response.ListResponse;
 import sd.majid.response.ObjectResponse;
 
@@ -24,64 +25,112 @@ public class OrganizationServiceUnitTests {
 	private IOrganizationService organizationService;
 	
 	@Test
-	public void addOrganizationUnitTest() {
-		ObjectResponse<Organization> response;
-		OrganizationDto organizationDto = new OrganizationDto();
-		organizationDto.setName("Majid Org");
-		organizationDto.setDescription("To make no one need food.");
-		organizationDto.setCreater_id(1L);
-		response = organizationService.addOrganization(organizationDto);
-		System.out.println(response.getResponseMessage());
-	}
-	
-	@Test
-	public void getOrganizationByIdUnitTest() {
-		ObjectResponse<Organization> response;
-		OrganizationDto organizationDto = new OrganizationDto();
-		organizationDto.setId(1L);
-		response = organizationService.getOrganizationById(organizationDto);
-		System.out.println(response.getResponseMessage() + " - " + response.getDto());
-	}
-	
-	@Test
-	public void declineOrganizationUnitTest() {
-		ObjectResponse<Organization> response;
-		OrganizationDto organizationDto = new OrganizationDto();
-		organizationDto.setId(1L);
-		organizationDto.setCreater_id(1L);
-		response = organizationService.declineOrganization(organizationDto);
-		System.out.println(response.getResponseMessage());
-	}
-	
-	@Test
-	public void activateOrganizationUnitTest() {
-		ObjectResponse<Organization> response;
-		OrganizationDto organizationDto = new OrganizationDto();
-		organizationDto.setId(1L);
-		organizationDto.setCreater_id(1L);
-		response = organizationService.activateOrganization(organizationDto);
-		System.out.println(response.getResponseMessage());
-	}
-	
-	@Test
-	public void getAllOrganizationUnitTest() {
-		ListResponse<Organization> response;
-		response = organizationService.getAllOrganization();
+	public void testAddOrganization() {
+		// Assess
+		User createdBy = new User();
+		createdBy.setId(1L);
+		Organization org = new Organization();
+		org.setName("Old Org");
+		org.setDescription("To collect food.");
+		org.setCreatedBy(createdBy);
+		
+		// Act
+		ObjectResponse<OrganizationDto> response;
+		response = organizationService.addOrganization(org);
+		
+		// Assert
 		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getAllActiveOrganizationUnitTest() {
-		ListResponse<Organization> response;
-		response = organizationService.getAllActiveOrganization();
+	public void testActivateOrganization() {
+		// Asses
+		User updateBy = new User();
+		updateBy.setId(2L);
+		Organization org = organizationService.getOrganizationObjectById(5L).getDto();
+		org.setUpdatedBy(updateBy);
+		
+		// Act
+		ObjectResponse<OrganizationDto> response;
+		response = organizationService.activateOrganization(org);
+		
+		// Assert
 		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getStatus());
 	}
 	
 	@Test
-	public void getAllDeclineOrganizationUnitTest() {
-		ListResponse<Organization> response;
-		response = organizationService.getAllDeclineOrganization();
+	public void testDeclineOrganization() {
+		// Asses
+		User updateBy = new User();
+		updateBy.setId(2L);
+		Organization org = organizationService.getOrganizationObjectById(3L).getDto();
+		org.setUpdatedBy(updateBy);
+		
+		// Act
+		ObjectResponse<OrganizationDto> response;
+		response = organizationService.declineOrganization(org);
+		
+		// Assert
 		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getStatus());
+	}
+	
+	@Test
+	public void testGetOrganizationById() {
+		ObjectResponse<OrganizationDto> response;
+		response = organizationService.getOrganizationById(1L);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getStatus());
+	}
+	
+	@Test
+	public void testGetOrganizationObjectById() {
+		ObjectResponse<Organization> response;
+		response = organizationService.getOrganizationObjectById(1L);
+		
+		// Assert
+		System.out.println(response);
+		System.out.println(response.getDto().getName() + " - " + response.getDto().getDescription() + " - " + response.getDto().getStatus());
+	}
+	
+	@Test
+	public void testGetAllActiveOrganization() {
+		ListResponse<OrganizationDto> response;
+		response = organizationService.getAllActiveOrganizations();
+		
+		// Assert
+		System.out.println(response);
+		for(OrganizationDto dto : response.getDtos()) {
+			System.out.println(dto.getName() + " - " + dto.getDescription() + " - " + dto.getStatus());
+		}
+	}
+	
+	@Test
+	public void testGetAllDeclineOrganization() {
+		ListResponse<OrganizationDto> response;
+		response = organizationService.getAllDeclinedOrganizations();
+		
+		// Assert
+		System.out.println(response);
+		for(OrganizationDto dto : response.getDtos()) {
+			System.out.println(dto.getName() + " - " + dto.getDescription() + " - " + dto.getStatus());
+		}
+	}
+	
+	@Test
+	public void testGetAllOrganization() {
+		ListResponse<OrganizationDto> response;
+		response = organizationService.getAllOrganizations();
+		
+		// Assert
+		System.out.println(response);
+		for(OrganizationDto dto : response.getDtos()) {
+			System.out.println(dto.getName() + " - " + dto.getDescription() + " - " + dto.getStatus());
+		}
 	}
 	
 }
